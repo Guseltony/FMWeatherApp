aa167fc026f7f3631f2af662114498318599c6eb
Akanji Anthony Guseltony <devguselt@gmail.com>
2025-09-17 16:30:18 +0100

    hero text and search button responsiveness implemented

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export const Search = () => {
   return (
-    <div className='flex items-center gap-16 w-[656px] h-[56px]'>
+    <div className='flex items-center gap-12 md:gap-16 w-[343px] md:w-[720px] lg:w-[656px] h-[124px] md:h-[56px] flex-col md:flex-row'>
       <div className='px-24 py-16 flex items-center gap-16 bg-neutral-800 rounded-12 w-[100%] h-[100%]'>
         <img src="/src/assets/images/icon-search.svg" alt="" />
         <input type="text" name="" id="" placeholder='Search for a place...' className='text-preset-5b font-medium border-0 outline-0' />
       </div>
-      <button type="submit" className='px-24 py-16 bg-blue-500 text-preset-5 font-medium rounded-12 cursor-pointer'>Search</button>
+      <button type="submit" className='px-24 py-16 bg-blue-500 text-preset-5 font-medium rounded-12 cursor-pointer w-[100%] md:w-fit'>Search</button>
     </div>
   )
 }
